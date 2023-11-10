fc89f9653f525c6e6c669d4e652c082cc27e743e
Rychu <jozek.melanczuk@gmail.com>
2023-11-10 01:13:58 +0100

    More matrix operations tests

--- NeuralNetwork/test/matrixTest/matrixOperationsTest.h
+++ NeuralNetwork/test/matrixTest/matrixOperationsTest.h
@@ -12,13 +12,27 @@ void testMatrixAddition(){
     std::cout << "MATRIX ADDITION TEST PASSED!\n";
 }
 
-void testMatrixMultiplication(){
+void mulTest1(){
     FastMatrix expectedResult(4,4, 16.0f);
     FastMatrix m1(4, 4, 2.0f);
     FastMatrix m2(4, 4, 2.0f);
     FastMatrix result = m1*m2;
     assert(result == expectedResult);
-    std::cout << "MATRIX MULTIPLICATION TEST PASSED!\n";
+    std::cout << "MATRIX MULTIPLICATION TEST 1 PASSED!\n";
+}
+
+void mulTest2(){
+    FastMatrix expectedResult(4,2, 16.0f);
+    FastMatrix m1(4, 4, 2.0f);
+    FastMatrix m2(4, 2, 2.0f);
+    FastMatrix result = m1*m2;
+    assert(result == expectedResult);
+    std::cout << "MATRIX MULTIPLICATION TEST 2 PASSED!\n";
+}
+
+void testMatrixMultiplication(){
+    mulTest1();
+    mulTest2();
 }
 
 void matrixOperationTest(){
