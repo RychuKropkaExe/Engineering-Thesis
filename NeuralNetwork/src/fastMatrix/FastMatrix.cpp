72e7601860dbf1a1cc9f53498a612ff7030e9d7d
Rychu <jozek.melanczuk@gmail.com>
2023-11-10 01:07:47 +0100

    Implementing basic functions and operators for FastMatrix

--- NeuralNetwork/src/fastMatrix/FastMatrix.cpp
+++ NeuralNetwork/src/fastMatrix/FastMatrix.cpp
@@ -1,12 +1,100 @@
 #include "FastMatrix.h"
 #include <iostream>
 using std::cout;
+using std::vector;
+#include <cassert>
+#include <cstdlib>
 
 FastMatrix::FastMatrix(size_t rows, size_t cols){
     this->rows = rows;
     this->cols = cols;
-    this->mat = std::make_unique<float[]>(rows*cols);
+    this->mat.reserve(rows*cols);
 }
+
+FastMatrix::FastMatrix(size_t rows, size_t cols, float val){
+    this->rows = rows;
+    this->cols = cols;
+    this->mat.reserve(rows*cols);
+    for(size_t i = 0; i < rows; ++i){
+        for(size_t j = 0; j < cols; ++j){
+            MAT_ACCESS((*this), i, j) = val;
+        }
+    }
+}
+
+float randomFloat()
+{
+    return (float)(rand()) / (float)(RAND_MAX);
+}
+
+void FastMatrix::randomize(){
+    for(size_t i = 0; i < rows; ++i){
+        for(size_t j = 0; j < cols; ++j){
+            MAT_ACCESS((*this), i, j) = randomFloat();
+        }
+    }
+}
+
+void FastMatrix::randomize(float low, float high){
+    for(size_t i = 0; i < rows; ++i){
+        for(size_t j = 0; j < cols; ++j){
+            MAT_ACCESS((*this), i, j) = low + randomFloat()*(high-low);
+        }
+    }
+}
+
+FastMatrix FastMatrix::operator+ (FastMatrix const& obj){
+    assert(rows == obj.rows);
+    assert(cols == obj.cols);
+    
+    FastMatrix result(rows,cols);
+
+    for(size_t i = 0; i < rows; ++i){
+        for(size_t j = 0; j < cols; ++j){
+            MAT_ACCESS(result, i, j) = MAT_ACCESS(*this, i, j) + MAT_ACCESS(obj, i, j);
+        }
+    }
+
+    return result;
+
+}
+
+FastMatrix FastMatrix::operator* (FastMatrix const& obj){
+    assert(rows == obj.rows);
+    assert(cols == obj.cols);
+    FastMatrix result(rows,cols, 0.0f);
+    for(size_t i = 0; i < rows; ++i){
+        for(size_t j = 0; j < obj.cols; ++j){
+            for(size_t k = 0; k < obj.rows; ++k){
+                MAT_ACCESS(result, i, j) += MAT_ACCESS(*this, i, k)*MAT_ACCESS(obj, k, j);
+            }
+        }
+    }
+
+    return result;
+
+}
+
+bool FastMatrix::operator== (FastMatrix const& obj){
+    if(rows != obj.rows)
+        return false;
+    if(cols != obj.cols)
+        return false;
+
+    for(size_t i = 0; i < rows; ++i){
+        for(size_t j = 0; j < cols; ++j){
+            if(MAT_ACCESS(*this, i, j) != MAT_ACCESS(obj, i, j))
+                return false;
+        }
+    }
+
+    return true;
+
+}
+
+// void randomizeFastMatrix(FastMatrix &mat){
+
+// }
 
 void printFastMatrix(FastMatrix &mat){
 
--- NeuralNetwork/src/fastMatrix/FastMatrix.h
+++ NeuralNetwork/src/fastMatrix/FastMatrix.h
@@ -2,19 +2,28 @@
 #define FAST_MATRIX_H
 
 #include <memory>
-using std::unique_ptr;
+#include <vector>
+using std::vector;
 
 #define MAT_ACCESS(FAST_MATRIX, I, J) (FAST_MATRIX).mat[(I)*(FAST_MATRIX).cols + (J)] 
 
 class FastMatrix{
-    
+
     public:
-        unique_ptr<float[]> mat;
+        vector<float> mat;
+        //[ 1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4 ]
         size_t rows;
         size_t cols;
 
         FastMatrix(size_t rows, size_t cols);
+        FastMatrix(size_t rows, size_t cols, float val);
 
+        FastMatrix operator+ (FastMatrix const& obj);
+        FastMatrix operator* (FastMatrix const& obj);
+        bool operator== (FastMatrix const& obj);
+
+        void randomize(float low, float high);
+        void randomize();
 };
 
 void printFastMatrix(FastMatrix &mat);
