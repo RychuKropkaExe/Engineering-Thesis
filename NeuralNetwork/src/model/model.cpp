b5fbb720cccce02d47823bff162a5c09fe2c544a
RychuKropkaExe <jozek.melanczuk@gmail.com>
2023-11-15 00:56:31 +0100

    Added dumping model to file, and parsing it from it

--- NeuralNetwork/src/model/model.cpp
+++ NeuralNetwork/src/model/model.cpp
@@ -1,7 +1,8 @@
 #include "model.h"
 #include <cassert>
 #include <iostream>
-
+#include <fstream>
+#include <sstream>      
 
 Model::Model(vector<size_t> arch, size_t archSize, vector<ActivationFunctionE> actFunctions, size_t actFunctionsSize, bool randomize){
     assert(archSize == actFunctionsSize);
@@ -27,6 +28,11 @@ Model::Model(vector<size_t> arch, size_t archSize, vector<ActivationFunctionE> a
 
     this->arch = arch;
     this->archSize = archSize;
+}
+
+Model::Model(){
+    this->layers.resize(1);
+    this->activationFunctions.resize(1);
 }
 
 void Model::setLearningRate(float val){
@@ -156,5 +162,139 @@ void Model::printModel(){
         printFastMatrix(layers[i].biases);
         std::cout << "ACTIVATION FUNCTION: " << layers[i].functionType << "\n";
     }
+
+}
+
+void Model::printModelToFile(std::string filename){
+    std::ofstream f(filename);
+
+  // Write to the file
+    f << numberOfLayers << "\n";
+    f << learningRate << "\n";
+    f << eps << "\n";
+    for(size_t i = 0; i < numberOfLayers; ++i){
+
+        f << layers[i].functionType << "\n";
+        f << layers[i].weights.rows << "\n";
+        f << layers[i].weights.cols << "\n";
+        for(size_t j = 0; j < layers[i].weights.rows*layers[i].weights.cols; ++j){
+            f << layers[i].weights.mat[j] << " ";
+        }
+        f << "\n";
+        f << layers[i].biases.rows << "\n";
+        f << layers[i].biases.cols << "\n";
+        for(size_t j = 0; j < layers[i].biases.rows*layers[i].biases.cols; ++j){
+            f << layers[i].biases.mat[j] << " ";
+        }
+        f << "\n";
+    }
+
+    // Close the file
+    f.close();
+}
+
+Model parseModelFromFile(std::string filename){
+    std::ifstream f(filename);
+    std::string buffer;
+
+    getline (f, buffer);
+    size_t numberOfLayers = (size_t)stoi(buffer);
+
+    getline (f, buffer);
+    float learningRate = (float)stof(buffer);
+
+    getline (f, buffer);
+    float eps = (float)stof(buffer);
+
+    vector<Layer> layers;
+    layers.resize(numberOfLayers);
+    for(size_t i = 0; i < numberOfLayers; ++i){
+
+        // f << layers[i].functionType << "\n";
+        // f << layers[i].weights.rows << "\n";
+        // f << layers[i].weights.cols << "\n";
+        // for(size_t j = 0; j < layers[i].weights.rows*layers[i].weights.cols; ++j){
+        //     f << layers[i].weights.mat[j] << " ";
+        // }
+        // f << "\n";
+        // f << layers[i].biases.rows << "\n";
+        // f << layers[i].biases.cols << "\n";
+        // for(size_t j = 0; j < layers[i].biases.rows*layers[i].biases.cols; ++j){
+        //     f << layers[i].biases.mat[j] << " ";
+        // }
+        // f << "\n";
+        getline (f, buffer);
+        ActivationFunctionE funcType = (ActivationFunctionE)stoi(buffer);
+        getline (f, buffer);
+        size_t weightsRows = (size_t)stoi(buffer);
+        getline (f, buffer);
+        size_t weightsCols = (size_t)stoi(buffer);
+        vector<float> wieghtsMat;
+        wieghtsMat.resize(weightsRows*weightsCols);
+        getline (f, buffer);
+        std::stringstream check(buffer);
+        //std::cout << buffer << "\n";
+
+        size_t counter = 0;
+
+        while(getline(check, buffer, ' '))
+        {   
+            //std::cout << buffer << "\n";
+            wieghtsMat[counter] = stof(buffer);
+            counter++;
+        }
+
+        getline (f, buffer);
+        size_t biasesRows = (size_t)stoi(buffer);
+        getline (f, buffer);
+        size_t biasesCols = (size_t)stoi(buffer);
+        vector<float> biasesMat;
+        biasesMat.resize(biasesCols*biasesRows);
+        
+        counter = 0;
+        getline (f, buffer);
+        std::cout << buffer << "\n";
+
+        std::stringstream check2(buffer);
+        while(getline(check2, buffer, ' '))
+        {
+            std::cout << buffer << "\n";
+            biasesMat[counter] = stof(buffer);
+            counter++;
+        }
+
+        FastMatrix weights;
+        FastMatrix biases;
+        FastMatrix output;
+        weights.cols = weightsCols;
+        weights.rows = weightsRows;
+        weights.mat = wieghtsMat;
+        biases.cols = biasesCols;
+        biases.rows = biasesRows;
+        biases.mat = biasesMat;
+        output.cols = biasesCols;
+        output.rows = biasesRows;
+        output.mat = biasesMat;
+
+        Layer l;
+
+        l.weights = weights;
+        l.biases = biases;
+        l.output = output;
+        l.functionType = funcType;
+
+        layers[i] = l;
+
+    }
+
+    Model model;
+
+    model.layers = layers;
+    model.numberOfLayers = numberOfLayers;
+    model.archSize = numberOfLayers;
+    model.learningRate = learningRate;
+    model.eps = eps;
+
+    return model;
 
 }
--- NeuralNetwork/src/model/model.h
+++ NeuralNetwork/src/model/model.h
@@ -8,20 +8,22 @@ using std::vector;
 
 class Model{
 
-    vector<Layer> layers;
-    size_t numberOfLayers;
-    vector<ActivationFunctionE> activationFunctions;
-
-    // For easy copy of model dimensions for finite difference purposes
-    vector<size_t> arch;
-    size_t archSize;
-
-    TrainingData trainingData {};
-    float learningRate = 1.f;
-    float eps = 1e-1;
-
     public:
+
+        vector<Layer> layers;
+        size_t numberOfLayers;
+        vector<ActivationFunctionE> activationFunctions;
+
+        // For easy copy of model dimensions for finite difference purposes
+        vector<size_t> arch;
+        size_t archSize;
+
+        TrainingData trainingData {};
+        float learningRate = 1.f;
+        float eps = 1e-1;
+
         Model(vector<size_t> arch, size_t archSize, vector<ActivationFunctionE> actFunctions, size_t actFunctionsSize, bool randomize);
+        Model();
 
         FastMatrix run(FastMatrix input);
 
@@ -34,7 +36,11 @@ class Model{
 
         void printModel();
 
+        void printModelToFile(std::string filename);
+
 
 };
+
+Model parseModelFromFile(std::string filename);
 
 #endif
