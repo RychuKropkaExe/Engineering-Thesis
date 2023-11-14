3b79fb8cb8cc085a89ce0a640f9d7546b8802a10
RychuKropkaExe <jozek.melanczuk@gmail.com>
2023-11-14 18:44:18 +0100

    Hamming length model test, added utils for test generation

--- NeuralNetwork/test/modelTest/generator.py
+++ NeuralNetwork/test/modelTest/generator.py
@@ -1,17 +1,33 @@
 
 
 
-for a in (0,1):
-    for b in (0,1):
-        for c in (0,1):
-            for d in (0,1):
-                for e in (0,1):
-                    for f in (0,1):
-                        for g in (0,1):
-                            for h in (0,1):
-                                print("{", f'{a}, {b}, {c}, {d}, {e}, {f}, {g}, {h}', "},")
+# for a in (0,1):
+#     for b in (0,1):
+#         for c in (0,1):
+#             for d in (0,1):
+#                 for e in (0,1):
+#                     for f in (0,1):
+#                         for g in (0,1):
+#                             for h in (0,1):
+#                                 print("{", f'{a}, {b}, {c}, {d}, {e}, {f}, {g}, {h}', "},")
 
-print("OUTPUT:")
+# print("OUTPUT:")
+# for a in (0,1):
+#     for b in (0,1):
+#         for c in (0,1):
+#             for d in (0,1):
+#                 for e in (0,1):
+#                     for f in (0,1):
+#                         for g in (0,1):
+#                             for h in (0,1):
+#                                 if h == 1:
+#                                     print("{0},")
+#                                 else:
+#                                     print("{1},")
+
+jumptable = ["{0, 0, 0},", "{0, 0, 1},", "{0, 1, 0},", "{0, 1, 1},", "{1, 0, 0},", "{1, 0, 1},", "{1, 1, 0},", "{1, 1, 1},"]
+
+
 for a in (0,1):
     for b in (0,1):
         for c in (0,1):
@@ -19,8 +35,13 @@ for a in (0,1):
                 for e in (0,1):
                     for f in (0,1):
                         for g in (0,1):
-                            for h in (0,1):
-                                if h == 1:
-                                    print("{0},")
-                                else:
-                                    print("{1},")
+                            print("{", f'{a}, {b}, {c}, {d}, {e}, {f}, {g}', "},")
+
+for a in (0,1):
+    for b in (0,1):
+        for c in (0,1):
+            for d in (0,1):
+                for e in (0,1):
+                    for f in (0,1):
+                        for g in (0,1):
+                            print(jumptable[a+b+c+d+e+f+g], )
